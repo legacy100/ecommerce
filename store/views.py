8f4582eb8c70fcdbee4c7391241faa4fcfad3c7b
shopapp <chikanwofor@gmail.com>
2022-08-20 12:11:33 +0100

    testing the templates

---
+++ store/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Product, Category
+
+
+def all_products(request):
+    products = Product.objects.all()
+    context = {
+        "products": products
+    }
+    return render(request, "store/home.html", context)
